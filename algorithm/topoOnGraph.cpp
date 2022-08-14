f264ff87e84574c3f41d2823a70350a0ee8dc7b4
swapfloor <106497594+swapfloor@users.noreply.github.com>
2022-08-14 21:23:01 +0800

    Update topoOnGraph.cpp

--- algorithm/topoOnGraph.cpp
+++ algorithm/topoOnGraph.cpp
@@ -21,3 +21,56 @@ int main(){
     }
   }
 }
+// 以前写这个版本太朴素了。后面出y总的版本
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N = 100005;
+
+int n, m;
+int h[N], e[N], ne[N], idx;
+int q[N];
+int d[N];
+
+void add(int a, int b) {
+    e[idx] = b, ne[idx] = h[a], h[a] = idx++;
+}
+
+bool topo() {
+    int hh = 0, tt = 0;
+    for (int i = 1; i <= n; i++) {
+        if (!d[i]) {
+            q[tt++] = i;//加入多源
+        }
+    }
+    while (hh < tt) {//队列
+        int t = q[hh++];
+        for (int i = h[t]; i != -1; i = ne[i]) {
+            int j = e[i];
+            if (--d[j] == 0) {
+                q[tt++] = j;
+            }
+        }
+    }
+    return tt == n;
+}
+
+int main() {
+    scanf("%d%d", &n, &m);
+    memset(h, -1, sizeof h);
+    while (m--) {
+        int a, b;
+        scanf("%d%d", &a, &b);
+        add(a, b);
+        d[b]++;
+    }
+    if (topo()) {
+        for (int i = 0; i < n; i++) {
+            printf("%d ", q[i]);
+        }
+        puts("");
+    } else {
+        puts("-1");
+    }
+}
